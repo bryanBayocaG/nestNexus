85670a7790145d55c561ac8f6e0ef7470ee4490c
Bryan Bayoca <bryanbayoca686@gmail.com>
2025-04-24 21:42:19 +0800

    pages data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
-function App() {
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/signIn";
+import AboutPage from "./pages/aboutPage";
+import SignOut from "./pages/signOut";
+import Profile from "./pages/profile";
+
+export default function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline bg-red-500">Hello world!</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
