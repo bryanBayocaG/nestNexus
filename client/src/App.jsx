65fd48f9c1514e13b09b0a2a2ea7b5302870dfed
Bryan Bayoca <bryanbayoca686@gmail.com>
2025-04-24 22:16:07 +0800

    header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import SignIn from "./pages/signIn";
 import AboutPage from "./pages/aboutPage";
 import SignOut from "./pages/signOut";
 import Profile from "./pages/profile";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function AboutPage() {
-  return <div></div>;
+  return <div>about</div>;
 }
 
 export default AboutPage;
