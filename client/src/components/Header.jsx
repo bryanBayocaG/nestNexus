73f7ebe86c51125dd95a21f5164a9752a2669359
Bryan Bayoca <bryanbayoca686@gmail.com>
2025-04-25 23:00:23 +0800

    fonts

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,10 +7,11 @@ const Header = () => {
   const [open, setOpen] = useState(false);
   return (
     <>
-      <header className="bg-white shadow-md rounded-2xl  mx-5 md:mx-10">
+      <header className="bg-white shadow-md rounded-2xl  mx-2 md:mx-10">
         <div className="flex justify-between items-center max-w-6xl mx-auto p-4">
           <Link to="/">
-            <h1 className="font-bold  text-sm sm:text-xl lg:text-2xl flex flex-wrap">
+            <h1 className="font-bold  text-sm sm:text-xl lg:text-2xl flex items-center flex-wrap">
+              <img className="w-12" src="/Noxus_Crest.png" alt="" />
               <span className="text-primary">Nests</span>
               <span className="text-secondary">Nexus</span>
             </h1>
@@ -19,7 +20,7 @@ const Header = () => {
             <input
               type="text"
               placeholder="Search..."
-              className="bg-transparent focus:outline-none w-40 md:w-60"
+              className="bg-transparent focus:outline-none w-40 md:w-50 lg:w-60"
             />
 
             <FaSearch className="text-secondary cursor-pointer" />
