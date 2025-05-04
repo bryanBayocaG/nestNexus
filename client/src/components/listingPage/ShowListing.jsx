6c3627cd54aa2ea0a9be8924b816b70c96516321
Bryan Bayoca <bryanbayoca686@gmail.com>
2025-05-04 23:39:17 +0800

    listing UI

--- client/src/components/listingPage/ShowListing.jsx
+++ client/src/components/listingPage/ShowListing.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { backEndBaseURL } from "../../utils/backendBaseURL";
 import { useSelector } from "react-redux";
+import { imageSrc } from "../../utils/imageAppwriteUrl";
+import { FaRegEdit } from "react-icons/fa";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 function ShowListing({ isActive }) {
   const [error, setError] = useState(null);
@@ -33,22 +36,49 @@ function ShowListing({ isActive }) {
       setError(error.message);
     }
   };
+  console.log(userListing);
   return (
     <>
       {error && <div className="text-red-500">{error}</div>}
-      {userListing.length > 0 &&
+      {userListing.length > 0 ? (
         userListing &&
         userListing.map((listing) => (
-          <div
-            key={listing._id}
-            className="border-2 border-gray-300 rounded-lg p-4 mb-4"
-          >
+          <div className="border-2 border-gray-200 rounded-lg p-4 mb-4">
+            <div className="relative mb-4">
+              <img
+                className="h-48 w-full object-cover rounded-lg mb-4"
+                src={imageSrc(listing.imageUrls[0])}
+                alt={listing.name}
+              />
+              <div
+                onClick={(e) => e.stopPropagation()}
+                className="opacity-0 hover:opacity-100 absolute bg-[rgba(0,0,0,0.5)] top-0 left-0 w-full h-full p-2 shadow-md gap-2 transition-all duration-300 ease-in-out"
+              >
+                <div className="relative w-full h-full">
+                  <div className="absolute top-2 right-2 flex gap-2">
+                    <button className="bg-blue-500 text-white rounded-md p-2 flex items-center gap-1">
+                      <FaRegEdit /> <span>Edit</span>
+                    </button>
+                    <button className="bg-red-500 text-white rounded-md p-2 flex items-center gap-1">
+                      <FaRegTrashAlt /> <span>Delete</span>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
             <h3 className="font-bold text-xl">{listing.name}</h3>
-            <p>{listing.description}</p>
             <p className="text-gray-500">Price: ${listing.regularPrice}</p>
             <p className="text-gray-500">Location: {listing.address}</p>
           </div>
-        ))}
+        ))
+      ) : (
+        <div className="flex flex-col justify-center items-center h-full">
+          <h1 className="text-2xl font-bold">No Listings Found</h1>
+          <p className="text-gray-500">
+            You have not created any listings yet.
+          </p>
+        </div>
+      )}
     </>
   );
 }
