1c97ecd213fed148e2b94db6b39fc8629bb63134
Bryan Bayoca <bryanbayoca686@gmail.com>
2025-05-04 19:51:52 +0800

    create listings api route

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import Listing from "../model/listing.model.js";
 import User from "../model/user.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
@@ -38,5 +39,18 @@ export const deleteUser = async (req, res, next) => {
     res.status(200).json({ message: "user deleted successfully!" });
   } catch (error) {
     next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, "You can only get your own listings!"));
+  } else {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
   }
 };
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -2,10 +2,13 @@ import express from "express";
 import {
   updateUserCredential,
   deleteUser,
+  getUserListings,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 router.post("/user_update/:id", verifyToken, updateUserCredential);
 router.delete("/delete/:id", verifyToken, deleteUser);
+
+router.get("/listings/:id", verifyToken, getUserListings);
 export default router;
