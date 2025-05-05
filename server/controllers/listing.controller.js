ca1f8367c8275c10ec26453f92246515326c0234
Bryan Bayoca <bryanbayoca686@gmail.com>
2025-05-05 22:40:40 +0800

    update listing api

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -32,3 +32,24 @@ export const deleteListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const updateListing = async (req, res, next) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+  if (req.user.id !== listing.userRef.toString()) {
+    return next(
+      errorHandler(403, "You are not authorized to delete this listing")
+    );
+  }
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
